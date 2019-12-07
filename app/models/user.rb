b4c05473929da6fd4e56767bd08642b38bb4854b
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2019-12-07 19:20:31 +0900

    create User test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments  # commentsテーブルとのアソシエーション
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+  
 end
