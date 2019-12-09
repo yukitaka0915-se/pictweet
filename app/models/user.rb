3486774304309162e230bd1baff17a508a7ebaac
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2019-12-09 20:23:14 +0900

    Tantai tesutokōdo no tsuika 11/5000 Adding unit test code
    create folder /spec/models
    create file /spac/models/user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :comments  # commentsテーブルとのアソシエーション
 
-  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :nickname, {presence: true, length: { maximum: 6 }}
+  # validates :email, {presence: true}
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+     end
+    it "is invalid without a email" do
+      user = User.new(nickname: "taikun", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
     end
   end
 end
