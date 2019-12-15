1abbc360a8708cdbf4dc5315b7e83ddebd4f8570
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2019-12-15 12:58:07 +0900

    create scenario 'post tweet'
    SSIA

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 feature 'tweet', type: :feature do
 # このブロックの内部にscenarioを記述していく
+  scenario 'post tweet' do
+    # ログイン前には投稿ボタンがない
+    visit root_path
+    expect(page).to have_no_content('投稿する')
+  end
 
 end
