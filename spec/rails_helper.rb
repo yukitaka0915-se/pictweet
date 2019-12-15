c3ae159f34b7c168d458eb2b65c8bdc1d95feabd
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2019-12-15 12:29:05 +0900

    add Capybara  Functions
    SSIA

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Capybaraの追加
+require 'capybara/rspec'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -33,6 +36,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Factory_botの記述省略
   config.include FactoryBot::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
